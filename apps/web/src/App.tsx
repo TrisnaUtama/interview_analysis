408dca18683403370c80a070b6dffdd456122f47
Trisna Utama <trisnautama@gmail.com>
2026-02-16 18:21:08 +0700

    test cicd pipeline

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1 className='text-9xl text-blue-400'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          HITUNG{count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
