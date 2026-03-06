4c9483027613d167139ccddff49c8edca235eec7
Trisna Utama <trisnautama@gmail.com>
2026-03-07 00:13:13 +0700

    feat: update cors

--- apps/api/internal/server/server.go
+++ apps/api/internal/server/server.go
@@ -45,7 +45,7 @@ func (s *Server) setupMiddleware() {
 	s.router.Use(middleware.Logger)
 	s.router.Use(middleware.Recoverer)
 	s.router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*.trisnautama.site", "http://localhost:*"},
+		AllowedOrigins:   []string{"https://*.trisnautama.com", "http://localhost:*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
 		AllowCredentials: true,
